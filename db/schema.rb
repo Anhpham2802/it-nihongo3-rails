7298e82b8b7b2eb76dbac5f26dae616f4da35ed2
anhpham <anhcherry2802@gmail.com>
2023-01-30 08:35:16 +0000

    Add profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
- ActiveRecord::Schema.define(version: 2023_01_12_155726) do
+ActiveRecord::Schema.define(version: 2023_01_30_050306) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,6 @@
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
   
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
